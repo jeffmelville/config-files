d5d1a20e7fcc9bd1b53503b1b8b5a5a0a1ba69c5
Jeff Melville <dev@jeffmelville.com>
2013-05-02 09:54:21 -0400

    Add .gitconfig

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 ln -sf $PWD/.vimrc $HOME/.vimrc
+ln -sf $PWD/.gitconfig $HOME/.gitconfig
 ln -sf $PWD/Preferences.sublime-settings $HOME/.config/sublime-text-2/Packages/User/
 ln -sf $PWD/Doxygen.sublime-snippet $HOME/.config/sublime-text-2/Packages/User/
