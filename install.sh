5dc2be31dced063b6d4b64b7de22c636cfb61b26
Jeff Melville <devSPAMNOT@jeffmelville.com>
2014-06-27 10:45:16 -0400

    Update install script.

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -sf $PWD/.vimrc $HOME/.vimrc
 ln -sf $PWD/.gitconfig $HOME/.gitconfig
 ln -sf $PWD/Preferences.sublime-settings $HOME/.config/sublime-text-2/Packages/User/
 ln -sf $PWD/Doxygen.sublime-snippet $HOME/.config/sublime-text-2/Packages/User/
+ln -sf $PWD/.tmux.conf $HOME/.tmux.conf
